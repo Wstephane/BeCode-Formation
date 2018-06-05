4f3b57476d499d406500c13b24c39e20d1f6c0c8
Wyns Stéphane <stewyns.pro@gmail.com>
2018-06-05 14:57:49 +0200

    Nouveau dossier

---
+++ Javascript/Entrainement Première Série/script.js
@@ -0,0 +1,37 @@
+// Excercice 1
+
+
+// var a = "J'aime le jambon";
+//
+// alert(a);
+
+
+// Excercice 2
+
+
+// var nom = "Wyns";
+// var prenom = "Stéphane";
+// var ville = "Bruxelles";
+//
+// alert("Nom" + " : " + nom + "\nPrénom" + " : " + prenom + "\nVille" + " : " + ville);
+
+
+//Excercice 3
+
+
+// var reponse = prompt("Quel est votre prénom svp ?", "<Entrez votre prénom ici");
+//
+// prompt("Bonjour," + reponse);
+
+
+// Excercice 4
+
+
+// var nom = prompt("Quel est ton nom ?");
+// var prenom = prompt("Quel est ton prénom ?");
+// var ville = prompt("Dans quel ville habites tu ?");
+//
+// alert("Nom : " + nom + "\nPrénom : " + prenom + "\nVille : " + ville);
+
+
+// Excercice 5
