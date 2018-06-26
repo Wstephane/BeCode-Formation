acc1dd1254de9d9a49916d20db46de14013f2c20
Wyns Stéphane <stewyns.pro@gmail.com>
2018-06-26 14:56:42 +0200

    Version 0.1

---
+++ Javascript/Entrainement Cinquième Série/questions.js
@@ -0,0 +1,81 @@
+/**
+ * Exercice sur les chaines de caractères.
+ * Trouvez la façon de faire la plus optimal.
+ * Il peut y avoir plusieur façon de faire.
+ */
+var tailleString = function (texte) {
+    return "Ce texte à une certaine taille".length;
+    // Ok
+}
+var remplaceECar = function (texte) {
+    "Ce texte à une certaine taille".length;
+   
+}
+var concatString = function (texte1, texte2) {
+    return a = "Le javascript est "+ "super";
+    // Ok
+}
+var afficherCar5 = function (texte) {
+    return "Ces exercices sont super!".charAt(4);
+    // Ok
+}
+var afficher9Car = function (texte) {
+    var a = "Je dirait";
+    return a.substr(0, 9);
+    // Ok
+}
+var majusculeString = function (texte) {
+    var a = "Un autre excellent exercice";
+    return a.toUpperCase();
+    // Ok
+}
+var minusculeString = function (texte) {
+    var a = "CELUI CI AUSSI!";
+    return a.toLowerCase();
+    // Ok
+}
+var SupprEspaceString = function (texte) {
+    var a = " Rome ne s'est pas construite en un jour ";
+    return a.trim();
+    // Ok
+}
+var IsString = function (texte) {
+    var a = "Hello";
+}
+
+var AfficherExtensionString = function (texte) {
+    var a = "images/photo01.jpg";
+    return a.split('.').pop();
+    // Ok
+}
+var NombreEspaceString = function (texte) {
+    var a = "Sire ouvrez on en a gros!";
+    return b = split(' ').length -1;
+    // Ok
+}
+var InverseString = function (texte) {
+    var a = "Après demain, à partir d\'aujourd\'hui?";
+
+}
+
+/**
+ * Exercices sur les nombres et les caluls mathématiques
+ */
+var calculPuissance = function (x, y) {
+
+}
+var valeurAbsolue = function (nombre) {
+
+}
+var valeurAbsolueArray = function (array) {
+
+}
+var sufaceCercle = function (rayon) {
+
+}
+var hypothenuse = function (ab, ac) {
+
+}
+var calculIMC = function (poids, taille) {
+
+}
