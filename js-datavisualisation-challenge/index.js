5032dc56ce85f7e50980550be6796f55a30c01cf
Wyns Stéphane <stewyns.pro@gmail.com>
2018-07-10 17:03:56 +0200

    Version 0.2

--- js-datavisualisation-challenge/index.js
+++ js-datavisualisation-challenge/index.js
@@ -1,8 +1,11 @@
+var t = document.getElementById("table1");
+var trh = t.getElementsByTagName("tr");
+var tableau = [];
+for(var i = 2 )
 
 
 
-
-var svg = dimple.newSvg("#table", 590, 400);
+var svg = dimple.newSvg("#table1", 590, 400);
     d3.tsv("assets/Crimes_recorded_by_the_police,_2002-12_YB14-fr.png", function (data) {
       data = dimple.filterData(data, "Owner", ["Aperture", "Black Mesa"])
       var myChart = new dimple.chart(svg, data);
