3c3f8e1c90d1940c125fbdc81881765c54f7050b
Wyns Stéphane <stewyns.pro@gmail.com>
2018-07-11 14:10:51 +0200

    Version 0.6

--- js-datavisualisation-challenge/index.js
+++ js-datavisualisation-challenge/index.js
@@ -1,23 +1,29 @@
 var t = document.getElementById("table1");
 var tbody = t.getElementsByTagName("tr");
+var trs = t.getElementsByTagName("tr")[1];
 var tableau = [];
 for(var i = 2; i < tbody.length; i++) {
-  for (var j = 2; i < tbody[0].length; j++) {
-
+  let element = tbody[i].getElementsByTagName("td");
+  for (var j = 1; j < element.length; j++) {
+  var obj = {
+  pays : element[0].innerHTML,
+  annee : tbody[1].getElementsByTagName("th")[j+1].innerHTML,
+  valeur : element[j].innerHTML,
+  }
+  console.log(obj);
   }
 }
 
 
-
-var svg = dimple.newSvg("#table1", 590, 400);
-    d3.tsv("assets/Crimes_recorded_by_the_police,_2002-12_YB14-fr.png", function (data) {
-      data = dimple.filterData(data, "Owner", ["Aperture", "Black Mesa"])
-      var myChart = new dimple.chart(svg, data);
-      myChart.setBounds(60, 30, 505, 305);
-      var x = myChart.addCategoryAxis("x", "Month");
-      x.addOrderRule("Date");
-      myChart.addMeasureAxis("y", "Unit Sales");
-      var s = myChart.addSeries("Channel", dimple.plot.area);
-      myChart.addLegend(60, 10, 500, 20, "right");
-      myChart.draw();
-    });
+// var svg = dimple.newSvg("#table1", 590, 400);
+//     d3.tsv("assets/Crimes_recorded_by_the_police,_2002-12_YB14-fr.png", function (data) {
+//       data = dimple.filterData(data, "Owner", ["Aperture", "Black Mesa"])
+//       var myChart = new dimple.chart(svg, data);
+//       myChart.setBounds(60, 30, 505, 305);
+//       var x = myChart.addCategoryAxis("x", "Month");
+//       x.addOrderRule("Date");
+//       myChart.addMeasureAxis("y", "Unit Sales");
+//       var s = myChart.addSeries("Channel", dimple.plot.area);
+//       myChart.addLegend(60, 10, 500, 20, "right");
+//       myChart.draw();
+//     });
