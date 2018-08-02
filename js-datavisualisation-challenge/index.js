0e1ddccc1b5c0bf9b2c719c10148926f52a6c306
Wyns Stéphane <stewyns.pro@gmail.com>
2018-08-02 16:28:14 +0200

    Version finaliser

--- js-datavisualisation-challenge/index.js
+++ js-datavisualisation-challenge/index.js
@@ -7,7 +7,7 @@ var tbody = t.getElementsByTagName("tr");
 var tableau = [];
 var obj = {};
 for(var i = 2; i < tbody.length; i++) {
-  let element = tbody[i].getElementsByTagName("td");
+  var element = tbody[i].getElementsByTagName("td");
   for (var j = 1; j < element.length; j++) {
     if(parseInt(element[j].innerHTML)){
       obj = {
@@ -28,15 +28,15 @@ $("#table1").before('<div id="chart1"></div>');
 
 var svg = dimple.newSvg("#chart1", "100%", 500);
 var myChart = new dimple.chart(svg, tableau);
-myChart.setBounds(50, 85, "90%", 350);
+var myLegend = myChart.addLegend(0, 5, 950, 200);
 var myAxisX = myChart.addCategoryAxis("x", ["annee", "pays"]);
+var myAxisY = myChart.addMeasureAxis("y", "valeur");
 myAxisX.title = "Année";
 myAxisX.fontSize = "auto";
-var myAxisY = myChart.addMeasureAxis("y", "valeur");
+myChart.setBounds(50, 85, "90%", 350);
 myAxisY.title = "Nombre en millier";
 myAxisY.fontSize = "auto";
 myChart.addSeries("pays", dimple.plot.line);
-var myLegend = myChart.addLegend(0, 5, 950, 200);
 myLegend.fontSize = "auto";
 myChart.draw();
 
@@ -49,7 +49,7 @@ var row = a.getElementsByTagName("tr");
 var tableau2 = [];
 var obj2 = {};
 for(var i = 1; i < row.length; i++) {
-  let val = row[i].getElementsByTagName("td");
+  var val = row[i].getElementsByTagName("td");
   for (var j = 0; j < val.length; j++) {
     if(parseInt(val[j].innerHTML)){
       obj2 = {
@@ -70,11 +70,11 @@ $("#table2").before('<div id="chart2"></div>');
 
 var svg2 = dimple.newSvg("#chart2", "100%", 600);
 var myChart2 = new dimple.chart(svg2, tableau2);
-myChart2.setBounds(50, 120, "90%", 350);
 var myAxisX = myChart2.addCategoryAxis("x", ["pays", "annee"]);
+var myAxisY = myChart2.addMeasureAxis("y", "valeur");
+myChart2.setBounds(50, 120, "90%", 350);
 myAxisX.title = "Pays";
 myAxisX.fontSize = "auto";
-var myAxisY = myChart2.addMeasureAxis("y", "valeur");
 myAxisY.title = "Nombre";
 myAxisY.fontSize = "auto";
 myChart2.addSeries("pays", dimple.plot.bar);
@@ -91,8 +91,8 @@ $("#firstHeading").after('<div id="chart3"></div>');
 
 var svg = dimple.newSvg("#chart3", "100%", 370);
 var myChart3 = new dimple.chart(svg, []);
-myChart3.setBounds(60, 30, "90%", 305)
 var x = myChart3.addCategoryAxis("x", "number");
+myChart3.setBounds(60, 30, "90%", 305)
 myChart3.addMeasureAxis("y", "variable");
 myChart3.addSeries("plop", dimple.plot.bubble);
 
@@ -101,25 +101,22 @@ myChart3.addSeries("plop", dimple.plot.bubble);
 var tableau3 = [];
 
 function call () {
-
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "https://inside.becode.org/api/v1/data/random.json");
   xhr.onload = function () {
-      var ourData = JSON.parse(xhr.responseText);
-
-  for(var i = 0 ; i < ourData.length; i++) {
-      obj3 = {
-        number : ourData[i][0],
-        variable : ourData[i][1],
-      }
-      tableau3.push(obj3);
+    var ourData = JSON.parse(xhr.responseText);
+    for(var i = 0 ; i < ourData.length; i++) {
+        obj3 = {
+          number : ourData[i][0],
+          variable : ourData[i][1],
+        }
+        tableau3.push(obj3);
+    }
   }
-}
-xhr.send();
-
-myChart3.data = tableau3;
-myChart3.draw();
-setTimeout(function(){call()}, 1000);
+  xhr.send();
+  myChart3.data = tableau3;
+  myChart3.draw();
+  setTimeout(function(){call()}, 1000);
 }
 
 call();
